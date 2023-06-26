0756e96443f463f83523c376dcde2ee54e316282
avtarvikas <avtarvikas236@gmail.com>
2023-06-26 16:55:55 +0530

    fix with social icon

--- src/components/navbar/Header.js
+++ src/components/navbar/Header.js
@@ -2,6 +2,8 @@ import React, { useEffect, useRef, useState } from "react";
 import Logo from "../../assets/image/SitmaticLogoNew.png";
 import "./header.css";
 import { menuItems } from "../../Data";
+import facebookIcon from "../../assets/image/facebook.svg";
+import twitterIcon from "../../assets/image/twitter.svg";
 import Menu from "./sub-menu/Menu";
 import BarIcon from "../../assets/image/bar.png";
 import Sidebar from "../sidebar/Sidebar";
@@ -35,43 +37,65 @@ const Header = () => {
     setValue(arr);
   };
   return (
-    <div className="headerContainer">
-      <div className="headerLogo">
-        <img src={Logo} className="logo" />
-      </div>
-      <ul className="headerList">
-        {menuItems.map((menu, i) => {
-          return (
-            <div
-              style={{ position: "relative" }}
-              onMouseEnter={() => onMouseEnter(i)}
-              onMouseLeave={() => onMouseLeave(i)}
-              key={i}
-            >
-              <li className="headerListItem">
-                <a className="listItem" href={menu.link}>
-                  {menu.title}
-                </a>
-
-                {menu.submenu && <div className="dash"></div>}
-
-                {dropdown[i] && <Menu items={menu.submenu} />}
-              </li>
+    <div className="header site-header">
+      <div className="siteheader-container">
+        <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} getValue={getValue} />
+        <div className="container headerContainer">
+          <div className="headerLogo">
+            <img src={Logo} className="logo" />
+          </div>
+          <div className="navBlock">
+            <div className="social-icon-block">
+              <a
+                href="https://www.facebook.com/Sitmatic"
+                className="social-icon-item"
+              >
+                <img src={facebookIcon} alt="F" width="15" />
+              </a>
+              <a
+                href="https://twitter.com/sitmatic"
+                className="social-icon-item"
+              >
+                <img src={twitterIcon} alt="T" width="15" />
+              </a>
             </div>
-          );
-        })}
-      </ul>
-      <div className="burgerIconBlock">
-        <img
-          src={BarIcon}
-          width={40}
-          onClick={() => {
-            getData();
-            setIsOpen(true);
-          }}
-        />
+            <div className="nav-item-block">
+              <ul className="headerList">
+                {menuItems.map((menu, i) => {
+                  return (
+                    <div
+                      style={{ position: "relative" }}
+                      onMouseEnter={() => onMouseEnter(i)}
+                      onMouseLeave={() => onMouseLeave(i)}
+                      key={i}
+                    >
+                      <li className="headerListItem">
+                        <a className="listItem" href={menu.link}>
+                          {menu.title}
+                        </a>
+
+                        {menu.submenu && <div className="dash"></div>}
+
+                        {dropdown[i] && <Menu items={menu.submenu} />}
+                      </li>
+                    </div>
+                  );
+                })}
+              </ul>
+              <div className="burgerIconBlock">
+                <img
+                  src={BarIcon}
+                  width={40}
+                  onClick={() => {
+                    getData();
+                    setIsOpen(true);
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} getValue={getValue} />
     </div>
   );
 };
--- src/home/Home.js
+++ src/home/Home.js
@@ -3,8 +3,17 @@ import Header from "../components/navbar/Header";
 
 const Home = () => {
   return (
-    <div>
+    <div className="">
       <Header />
+      <div className="container main-container">
+        <iframe
+          src="https://configurator.ml3ds-icon.com/3/series"
+          name="myIFrame"
+          scrolling="auto"
+          width="100%"
+          height="1700px"
+        />
+      </div>
     </div>
   );
 };
