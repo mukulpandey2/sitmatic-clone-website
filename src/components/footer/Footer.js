62b1bc9ed6e157480e8185bf94151f1204332b4c
avtarvikas <avtarvikas236@gmail.com>
2023-06-26 22:57:50 +0530

    fixed responsive

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footerContainer">
+      <div className="container">
+        <h3>Archives</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
