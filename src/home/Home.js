19d3cd09a8e9140502b88d8cebcba6e127c6c7ba
mukulpandey2 <pandeymukul0000@gmail.com>
2024-01-12 16:32:25 +0530

    minor changes

--- src/home/Home.js
+++ src/home/Home.js
@@ -8,13 +8,7 @@ const Home = () => {
     <div className="">
       <Header />
       <div className="container main-container">
-        <iframe
-          src="https://configurator.ml3ds-icon.com/3/series"
-          name="myIFrame"
-          scrolling="auto"
-          width="100%"
-          height="1700px"
-        />
+       <h1>hello</h1>
       </div>
       <GoToTop />
       <Footer />
